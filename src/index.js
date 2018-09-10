e2fbc9c2950eeab5837d0c8a3fdfa84e696cea84
Disa0808 <levshukdenis@mail.ru>
2018-09-11 00:54:47 +0300

    First

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for(var i = 0; i < preferences.length - 1; i++ ) {
+    if (preferences[i] === i + 1) {
+      preferences[i] = -preferences[i];
+    } 
+    if (preferences[preferences[preferences[i] - 1] - 1] - 1 === i) {
+      count++;
+      preferences[i] = -1;
+      preferences[preferences[i] - 1] = -1;
+      preferences[preferences[preferences[i] - 1] - 1] = -1;
+    }
+  }
+  return count;
 };
